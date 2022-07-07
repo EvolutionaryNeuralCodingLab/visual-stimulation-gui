b9ef70f2380113d02fde9e3d95c771dd4a2abe76
SheinIdelsonMark <shein.mark@gmail.com>
2022-07-07 09:33:47 +0300

    Update VS_spatialNoise.m
    Update vstim with new triggers and rearrange function

--- VStim/SheinIdelsonStims/VS_spatialNoise.m
+++ VStim/SheinIdelsonStims/VS_spatialNoise.m
@@ -1,4 +1,4 @@
-classdef VS_spatialNoise < VStim
+classdef VS_spatialNoise < VStim %modified 6/6/22
     properties
         %all these properties are modifiable by user and will appear in visual stim GUI
         %Place all other variables in hidden properties
@@ -9,12 +9,11 @@ classdef VS_spatialNoise < VStim
         numberOfFrames=450
         frameRate=45;
         sparsity=30;
-        
     end
     properties (Hidden,Constant)
-        defaultTrialsPerCategory=500; %number of gratings to present
+        defaultTrialsPerCategory=2; %number of gratings to present
         defaultBackground=128;
-         defaultITI=0;
+        defaultITI=0;
         meanLuminosityTxt='luminance value for grey pixels';
         contrastTxt='% of dynamic range to use';
         rectNumTxt='How many rectangles to put onto each spatial dimension (not counting the mask)';
@@ -24,12 +23,11 @@ classdef VS_spatialNoise < VStim
         remarks={''};
     end
     properties (Hidden)
-        stim
+        allNoisePatterns
         stimOnset
         flipOffsetTimeStamp
         flipMiss
         flipOnsetTimeStamp
-        
     end
     methods
         function obj=run(obj)
@@ -52,26 +50,28 @@ classdef VS_spatialNoise < VStim
             goodPix=(screenProps.width/2-pixY).^2+(screenProps.height/2-pixX).^2<stimRadious^2;
             whDiff=(screenProps.width-screenProps.height)/2;
             
-            %see whether the different rectangle positions overlap with the
-            %non-good pixels
-            for x=1:length(edges)
-                for y=1:length(edges)
-                    toInclude(x,y)=min(min(goodPix(edges(y):(edges(y)+rectSpacing),(edges(x)+whDiff):(edges(x)+rectSpacing+whDiff))));
+            %see whether the different rectangle positions overlap with the presentation area
+            if ~obj.showOnFullScreen
+                for x=1:length(edges)
+                    for y=1:length(edges)
+                        toInclude(x,y)=min(min(goodPix(edges(y):(edges(y)+rectSpacing),(edges(x)+whDiff):(edges(x)+rectSpacing+whDiff))));
+                    end
                 end
+                toInclude=reshape(toInclude,size(toInclude,1)*size(toInclude,2),1);
+            else
+                toInclude=true(actRectNum^2,1);
             end
-            toInclude=reshape(toInclude,size(toInclude,1)*size(toInclude,2),1);
-            
             
             %what pixel value for each
             whiteVal=round(obj.meanLuminosity-obj.contrast*obj.meanLuminosity)+1;
             blackVal=round(obj.meanLuminosity+obj.contrast*obj.meanLuminosity)-1;
             greyVal=obj. meanLuminosity;
+
             %generate stimulus
             rectPos=zeros(screenProps.height,screenProps.width);
-            obj.stim=cell(obj.trialsPerCategory,obj.numberOfFrames);
+            obj.allNoisePatterns=zeros(actRectNum,actRectNum,obj.trialsPerCategory,obj.numberOfFrames);
             for t=1:obj.trialsPerCategory
                 for x=1:obj.numberOfFrames
-                    
                     intensity=rand(actRectNum^2,1);
                     w=(intensity<0.01*obj.sparsity/2);
                     b=(intensity>=0.01*obj.sparsity/2);
@@ -81,11 +81,9 @@ classdef VS_spatialNoise < VStim
                     intensity(g)=greyVal;
                     intensity(toInclude==0)=greyVal;
                     rectPos=reshape(intensity,size(intensity,1)^0.5,size(intensity,1)^0.5);
-                    obj.stim{t,x}=rectPos;
-                    tex(t,x)=Screen('MakeTexture', obj.PTB_win, rectPos);
+                    obj.allNoisePatterns(:,:,t,x)=rectPos;
                 end
             end
-            
             
             %initialize for post hoc monitering of stimuuls timing
             obj.flipOnsetTimeStamp=nan(obj.trialsPerCategory,obj.numberOfFrames+1); %when the flip happened
@@ -93,7 +91,8 @@ classdef VS_spatialNoise < VStim
             obj.flipOffsetTimeStamp=nan(obj.trialsPerCategory,obj.numberOfFrames+1);  %flip done
             obj.flipMiss=nan(obj.trialsPerCategory,obj.numberOfFrames+1);              %positive for missed stim
             
-            
+            tex=obj.prepareTextures(1);
+
             save tmpVSFile obj; %temporarily save object in case of a crash
             disp('Session starting');
             
@@ -102,12 +101,11 @@ classdef VS_spatialNoise < VStim
             winRect=[round(screenProps.width/2)-round(screenProps.height/2) 0 screenProps.height+round(screenProps.width/2)-round(screenProps.height/2) screenProps.height];  %change this!
             
             %main loop - start the session
-            %pp(uint8(obj.trigChNames(1)),true,false,uint8(0),uint64(32784)); %session start trigger (also triggers the recording start)
+            obj.sendTTL(1,true); %session start trigger (also triggers the recording start)
             WaitSecs(obj.preSessionDelay); %pre session wait time
-            pp(uint8(obj.trigChNames(1)),true,false,uint8(0),uint64(32784)); %session start trigger (also triggers the recording start)
             
             for i=1:obj.trialsPerCategory
-                pp(uint8(obj.trigChNames(2)),true,false,uint8(0),uint64(32784)); %session start trigger (also triggers the recording start)
+                obj.sendTTL(2,true); %trial start trigger
                 
                 disp(['Trial ' num2str(i) '/' num2str(obj.trialsPerCategory)]);
                 objRect = SetRect(0,0, obj.rectNum, obj.rectNum);
@@ -115,17 +113,13 @@ classdef VS_spatialNoise < VStim
                 count = 1;
                 
                 while count <= obj.numberOfFrames
-                    
-                      
                     Screen('DrawTexture', obj.PTB_win, tex(i,count), [], dstRect, [], 0);
-                    Screen('DrawTexture',obj.PTB_win,obj.masktex);
                     WaitSecs(1/obj.frameRate);
-                     pp(uint8(obj.trigChNames(3)),true,false,uint8(0),uint64(32784)); %session start trigger (also triggers the recording start)
+                    obj.applyBackgound;  %set background mask and finalize drawing (drawing finished)
+                    obj.sendTTL(3,true); %frame start trigger
                     [obj.flipOnsetTimeStamp(i,count),obj.stimOnset(i,count),obj.flipOffsetTimeStamp(i,count),obj.flipMiss(i,count)]=Screen('Flip',obj.PTB_win);
-                    pp(uint8(obj.trigChNames(3)),false,false,uint8(0),uint64(32784)); %session start trigger (also triggers the recording start)
-                    Screen('DrawingFinished', obj.PTB_win); % Tell PTB that no further drawing commands will follow before Screen('Flip')
+                    obj.sendTTL(3,true); %frame end trigger
                     count = count + 1;
-                    
                 end
                 
                 %check if stimulation session was stopped by the user
@@ -135,10 +129,10 @@ classdef VS_spatialNoise < VStim
                     Screen('FillOval',obj.PTB_win,obj.visualFieldBackgroundLuminance);
                     Screen('DrawTexture',obj.PTB_win,obj.masktex);
                     [obj.flipOnsetTimeStamp(i,count),obj.stimOnset(i,count),obj.flipOffsetTimeStamp(i,count),obj.flipMiss(i,count)]=Screen('Flip',obj.PTB_win);
-                    pp(uint8(obj.trigChNames(2)),false,false,uint8(0),uint64(32784)); %session start trigger (also triggers the recording start)
+                    obj.sendTTL(2,false); %frame start trigger
                     WaitSecs(obj.interTrialDelay);
                     disp('Trial ended early');
-                    pp(uint8(obj.trigChNames(1)),false,false,uint8(0),uint64(32784)); %session start trigger (also triggers the recording start)
+                    obj.sendTTL(1,false); %frame start trigger
                     WaitSecs(obj.postSessionDelay);
                     disp('Session ended');
                     
@@ -146,24 +140,32 @@ classdef VS_spatialNoise < VStim
                 end
                 
                 Screen('FillOval',obj.PTB_win,obj.visualFieldBackgroundLuminance);
-                Screen('DrawTexture',obj.PTB_win,obj.masktex);
+                obj.applyBackgound;  %set background mask and finalize drawing (drawing finished)
                 [obj.flipOnsetTimeStamp(i,count),obj.stimOnset(i,count),obj.flipOffsetTimeStamp(i,count),obj.flipMiss(i,count)]=Screen('Flip',obj.PTB_win);
-                pp(uint8(obj.trigChNames(2)),false,false,uint8(0),uint64(32784)); %session start trigger (also triggers the recording start)
+                obj.sendTTL(2,false); %frame start trigger
+                
+                %prepare next stim
+                tex=obj.prepareTextures(i+1);
+
                 WaitSecs(obj.interTrialDelay);
                 disp('Trial ended');
-                                
             end
             
             Screen('FillOval',obj.PTB_win,obj.visualFieldBackgroundLuminance);
-            Screen('DrawTexture',obj.PTB_win,obj.masktex);
+            obj.applyBackgound;  %set background mask and finalize drawing (drawing finished)
             Screen('DrawingFinished', obj.PTB_win); % Tell PTB that no further drawing commands will follow before Screen('Flip')
-            pp(uint8(obj.trigChNames(1)),false,false,uint8(0),uint64(32784)); %session start trigger (also triggers the recording start)
+            obj.sendTTL(1,false); %frame start trigger
             WaitSecs(obj.postSessionDelay);
             
             disp('Session ended');
             
         end
-        
+
+        function tex=prepareTextures(obj,trialNumber)
+            for x=1:obj.numberOfFrames
+               tex(x)=Screen('MakeTexture', obj.PTB_win, obj.allNoisePatterns(:,:,trialNumber,x));
+            end
+        end
         
         %class constractor
         function obj=VS_spatialNoise(w,h)
@@ -171,7 +173,7 @@ classdef VS_spatialNoise < VStim
             %get the visual stimulation methods
             obj.trialsPerCategory=obj.defaultTrialsPerCategory;
             obj.visualFieldBackgroundLuminance=obj.defaultBackground;
-              obj.interTrialDelay=obj.defaultITI;
+            obj.interTrialDelay=obj.defaultITI;
         end
         
     end
